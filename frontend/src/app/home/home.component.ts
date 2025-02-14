23c1d92b79ace0e9b1f9a3c855f47d264a1fb905
biboy-al <alvien.salvador@outlook.com>
2025-02-14 23:35:31 +1300

    reorg. the folder names

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AboutMeComponent } from '../components/about-me/about-me.component';
 import { ContentContainerComponent } from '../components/content-container/content-container.component';
+import { SkillComponent } from '../components/skill/skill.component';
+import { ProjectComponent } from '../components/project/project.component';
 
 @Component({
   selector: 'app-home',
-  imports: [AboutMeComponent, ContentContainerComponent],
+  imports: [AboutMeComponent, ContentContainerComponent,SkillComponent, ProjectComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
