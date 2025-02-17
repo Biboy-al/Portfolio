3368e366ec95bab840079b5ef74af0e4071e3d49
biboy-al <alvien.salvador@outlook.com>
2025-02-17 22:12:13 +1300

    added API and database integration

--- server/server.js
+++ server/server.js
@@ -1,2 +1,44 @@
 const express = require('express')
 const app = express()
+const PORT = 8080
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://biboy:<db_password>@portfolio.t45iz.mongodb.net/?retryWrites=true&w=majority&appName=portfolio";
+
+app.listen(
+    PORT,
+    () => console.log(`Sever is listening on http://localhost:${PORT}`)
+)
+
+app.get( '/skills', (req, res) =>{
+    res.status(200).send({
+        "hi":"hi"
+    })
+})
+
+app.get( '/projects', (req, res) =>{
+    res.send('Obtaining projects')
+})
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
