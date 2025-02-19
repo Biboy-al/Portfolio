7f1ff90582192738de205929ddb8b44e7f98bcda
biboy-al <alvien.salvador@outlook.com>
2025-02-19 17:39:59 +1300

    Making a GET request to the /skills end point now retrives skills from mongoDB

--- server/routes/skills.js
+++ server/routes/skills.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const Skill = require('../models/skills');
 const router = express.Router();
 
 
-router.get( '/', (req, res) =>{
-    res.status(200).send({
-        "hi":"hi"
-      })
+router.get( '/', async (req, res) =>{
+    res.status(200).send(
+      await Skill.find()
+    )
 });
 
+router.post( '/', (req, res) =>{
+  res.status(200).send({
+      "hi":"hi"
+    })
+});
+
+router.delete( '/', (req, res) =>{
+  res.status(200).send({
+      "hi":"hi"
+    })
+});
+
+router.put( '/', (req, res) =>{
+  res.status(200).send({
+      "hi":"hi"
+    })
+});
 
 module.exports = router;
 
